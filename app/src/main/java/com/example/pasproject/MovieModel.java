b77903ae1c4853c1d24ae5828268fb7aae92a023
“altharaa” <aletheamaureen01@gmail.com>
2023-05-27 09:47:54 +0700

    menambahkan halaman login

--- app/src/main/java/com/example/pasproject/MovieModel.java
+++ app/src/main/java/com/example/pasproject/MovieModel.java
@@ -1,8 +1,31 @@
 package com.example.pasproject;
 
 public class MovieModel {
-    String moTitle;
-    String mPoster;
-    String mlanguage;
-    String mOverview;
+    private String movieName;
+    private String releasedDate;
+    private String mvPoster;
+    private String mvOverview;
+
+    public String getMovieName() {return movieName;}
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getReleasedDate() {
+        return releasedDate;
+    }
+
+    public void setReleasedDate(String releasedDate) {
+        this.releasedDate = releasedDate;
+    }
+
+    public String getMvPoster() {return mvPoster;}
+
+    public void setMvPoster(String mvPoster) {this.mvPoster = mvPoster;}
+
+    public String getMvOverview() {return mvOverview;}
+
+    public void setMvOverview(String overview) {this.mvOverview = overview;}
+
 }

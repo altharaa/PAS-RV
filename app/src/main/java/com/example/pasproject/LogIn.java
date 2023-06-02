178baaa746de86d39274ee7e2b1060a721481936
“altharaa” <aletheamaureen01@gmail.com>
2023-06-02 23:30:19 +0700

    menambahkan id

--- app/src/main/java/com/example/pasproject/LogIn.java
+++ app/src/main/java/com/example/pasproject/LogIn.java
@@ -2,13 +2,27 @@ package com.example.pasproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class LogIn extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
+    public static final String SHARED_PREFS = "shared_prefs";
+
+    public static final String EMAIL_KEY = "email_key";
+
+    public static final String PASSWORD_KEY = "password_key";
+
+    SharedPreferences sharedpreferences;
+    String email, password;
+
+    EditText et_email;
+
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login);
+//    }
 }
